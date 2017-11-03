14aee4413fd3cc0e24511a59c0dcab7aeedd2d9f
Milan Kragujević <milankragujevic@gmail.com>
2017-11-03 14:02:48 +0100

    dump

---
+++ src/js/player.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const $ = require('jquery')
+
+$(() => {
+	let data = {
+		type: 'PARATII_COMM',
+		payload: {
+			action: 'start',
+			uri: window.location.hash.replace(/#/g, '')
+		}
+	} 
+	window.postMessage(data, '*')
+	window.addEventListener('message', (event) => {
+		if (event.data.type && event.data.type == 'PARATII_RESP') {
+			console.log('Received message from extension...')
+			console.log(event.data)
+		}
+		if (event.data.type && event.data.type == 'PARATII_DATA') {
+			console.log('Received chunk data from extension...')
+			let chunk = event.data.data
+		}
+	})
+})
